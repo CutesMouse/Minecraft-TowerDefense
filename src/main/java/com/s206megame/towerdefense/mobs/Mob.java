9529ee2d20d883b4d42107879ba855650b5ab1a8
CutesMouse <hfbubble@gmail.com>
2021-04-22 10:59:30 +0800

    2021/4/22 - 怪物移動

--- src/main/java/com/s206megame/towerdefense/mobs/Mob.java
+++ src/main/java/com/s206megame/towerdefense/mobs/Mob.java
@@ -1,10 +1,14 @@
 package com.s206megame.towerdefense.mobs;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 
 public interface Mob {
+    Entity getEntity();
     String getMobName();
     String getMobID();
     void onEntityMove();
     void spawn(Location loc);
+    void moveMob(double distance);
+    void setFacingDegree(double yaw);
 }
